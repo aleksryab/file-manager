0bc3e7ed29e0d19ae94cb8cbc4624caabd97127e
aleksryab <aleksryabtsev@gmail.comm>
2023-06-25 16:09:51 +0700

    feat: implement read command interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('Start!');
+import * as readline from 'node:readline/promises';
+import getUserName from './helpers/getUserName.js';
+
+const EXIT_COMMAND = '.exit';
+
+const start = () => {
+  const userName = getUserName(process.argv);
+  console.log(`Welcome to the File Manager, ${userName}!`);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.prompt();
+  rl.on('line', (input) => {
+    const command = input.trim();
+    if (command === EXIT_COMMAND) {
+      rl.close();
+    } else {
+      console.log('Command');
+      rl.prompt();
+    }
+  });
+
+  rl.on('close', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit(0);
+  });
+};
+
+start();
